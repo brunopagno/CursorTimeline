9ced4caa97704327c4932686de8fa3bfd3464cc4
Bruno Pagno <brunopagno@gmail.com>
2014-09-22 18:14:45 -0300

    Updated cursor positioning and removed unused code

--- Assets/Scripts/LOPCursor.cs
+++ Assets/Scripts/LOPCursor.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LOPCursor : MonoBehaviour {
 
     private Quaternion rotation;
     private Quaternion initialRotation;
     private bool calibrate = true;
+    private Queue<Vector3> smoothingQueue = new Queue<Vector3>(16);
 
     public GameObject rotationMaster;
 
     public GameObject cursorProj;
-    public Plane referencePlane;
+    public Plane referencePlaneHorizontal;
+    public Plane referencePlaneVertical;
     public int distance = 15;
 
     void Start() {
-        referencePlane = new Plane(Vector3.back, Vector3.zero);
+        referencePlaneHorizontal = new Plane(Vector3.down, Vector3.zero);
+        referencePlaneVertical = new Plane(Vector3.back, Vector3.zero);
 
         if (Network.isClient) {
             if (SystemInfo.supportsGyroscope) {
@@ -45,20 +49,47 @@ public class LOPCursor : MonoBehaviour {
 
     void Update() {
         if (Network.isServer) {
-            Vector3 direction = rotation * Vector3.forward;
-            Ray ray = new Ray(Vector3.back * distance, direction);
+            Vector3 direction = rotation * Vector3.up;
+            Ray ray = new Ray(Vector3.down * distance, direction);
             float rayDistance = 0;
-            if (referencePlane.Raycast(ray, out rayDistance)) {
-                Vector3 result = ray.GetPoint(rayDistance);
-                cursorProj.transform.position = new Vector3(result.x, result.y, 0);
+            float x = 0;
+            if (referencePlaneHorizontal.Raycast(ray, out rayDistance)) {
+                x = ray.GetPoint(rayDistance).x;
             }
+
+            direction = rotation * Vector3.forward;
+            ray = new Ray(Vector3.back * distance, direction);
+            rayDistance = 0;
+            float y = 0;
+            if (referencePlaneVertical.Raycast(ray, out rayDistance)) {
+                y = -ray.GetPoint(rayDistance).y;
+            }
+
+            smoothingQueue.Enqueue(new Vector3(x, y, 0));
+            if (smoothingQueue.Count >= 8) {
+                smoothingQueue.Dequeue();
+            }
+
+            cursorProj.transform.position = MediumPosition();
         }
+    }
+
+    private Vector3 MediumPosition() {
+        float x = 0;
+        float y = 0;
+        foreach (Vector3 v in smoothingQueue.ToArray()) {
+            x += v.x;
+            y += v.y;
+        }
+
+        return new Vector3(x / smoothingQueue.Count, y / smoothingQueue.Count, 0);
     }
 
     void OnGUI() {
         if (Network.isClient) {
-            GUILayout.Label("");
-            GUILayout.Label("Attitude: " + Input.gyro.attitude);
+            if (GUI.Button(new Rect(10, 150, 150, 100), "recalibrate")) {
+                calibrate = true;
+            }
         }
     }
 }
